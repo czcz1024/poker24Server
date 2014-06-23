d768cb9d52375025854d44c005b435d04212547b
czcz1024@google.com <czcz1024@gmail.com>
2014-06-23 20:30:01 +0800

    xx

---
+++ Poke24Server/Hubs/TabHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Poke24Server.Hubs
+{
+    public class TabHub : Hub
+    {
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
+        public void Ready()
+        {
+        }
+
+        public void Say(string str)
+        {
+        }
+
+        public void Pass()
+        {
+
+        }
+
+        public void Poke(string hand)
+        {
+        }
+
+    }
+}
